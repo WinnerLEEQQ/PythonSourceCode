92430086b2a266efb929202c98647e8e2d5c3606
WinnerLEE <74951841+WinnerLEEQQ@users.noreply.github.com>
2021-07-20 00:46:14 +0800

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 16 15:58:48 2021
+
+@author: winner
+"""
+
+column, row = 3, 5
+array2D = [[0 for _ in range(row)] for _ in range(column)]
+print(array2D)
+
+'''
+[[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+'''
+
+array2D = [[0 for _ in range(row)]]
+print(array2D)
+
+'''
+[[0, 0, 0, 0, 0]]
+'''
+
+#array2D = [[for _ in range(row)]]
+#print(array2D)
+
+'''
+[[], [], [], [], []]
+'''
